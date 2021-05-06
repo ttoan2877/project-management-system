63bffffdd2a66d0d6b5cfd9c3e646e8ad8ef6d92
Minh Nguyen <minh.nguyenhoang2401@gmail.com>
2021-05-06 19:25:54 +0700

    fix conflict

--- projectmanager_client/app/screens/Tasks/index.tsx
+++ projectmanager_client/app/screens/Tasks/index.tsx
@@ -1,64 +1,11 @@
 import React, { useEffect, Fragment } from 'react'
-import { FlatList, StyleSheet } from 'react-native'
+import { FlatList, StyleSheet, View } from 'react-native'
 import LayoutPrimary from 'components/LayoutPrimary'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Tasks = () => {
-  const dispatch = useDispatch()
-  const { isLoading , data} = useSelector(getProjectState)
-
-  const onProject = useCallback(
-    (ID: number) => {
-      dispatch(fetchProject(ID))
-    },
-    [dispatch],
-  )
-
-  const onRefresh = useCallback(() => {
-    dispatch(fetchUserProject())
-  }, [dispatch])
-
-  useEffect(() => {
-    onRefresh()
-  }, [onRefresh])
-
-  const renderItem = useCallback(
-    ({ item }) => {
-      const isOwner = get(item, 'creator_id') === ID
-      return (
-        <Card
-          name={get(item, 'name', '')}
-          description={get(item, 'description', '')}
-          created_at={get(item, 'CreatedAt', '')}
-          users={get(item, 'users', [])}
-          isOwner={isOwner}
-          onPress={() => onProject(get(item, 'ID'))}
-        />
-      )
-    },
-    [ID, onProject],
-  )
-  useEffect(() => {}, [])
-  return <LayoutPrimary title="Choose your project" renderFooter={renderFooter}>
-  <Fragment>
-    <FlatList
-      showsVerticalScrollIndicator={false}
-      // onRefresh={onRefresh}
-      // refreshing={isLoading}
-      style={styles.container}
-      contentContainerStyle={[
-        !projects?.length && styles.container,
-        styles.padding,
-      ]}
-      data={projects}
-      keyExtractor={(item, index) => index.toString()}
-      renderItem={renderItem}
-      ListEmptyComponent={ListEmptyComponent}
-      ItemSeparatorComponent={Separator}
-    />
-    <Loading isLoading={isLoading} />
-  </Fragment>
-</LayoutPrimary>
+  return <View />
+}
 
 export default Tasks
 
