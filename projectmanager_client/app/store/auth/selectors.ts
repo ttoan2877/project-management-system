b602e2fc0d177678b7d8cfbffa7eecf25a15b2d9
Minh Nguyen <minh.nguyenhoang2401@gmail.com>
2021-05-08 11:12:58 +0700

    crash on auth data

--- projectmanager_client/app/store/auth/selectors.ts
+++ projectmanager_client/app/store/auth/selectors.ts
@@ -1,5 +1,5 @@
 import { get } from 'lodash'
 
 export const getAuthState = (state: any) => state.auth
-export const getAuthData = (state: any) => get(state, 'auth.data')
+export const getAuthData = (state: any) => get(state, 'auth.data' {})
 export const tokenGetter = (state: any) => get(state, 'auth.data.token')
